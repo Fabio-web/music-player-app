778094ea2a68db23fb526338feea097f0ac23f0d
Fabio-web <fabio.maccotta@ynov.com>
2021-12-30 23:25:41 +0100

    + colors constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color kBackground = Color(0xFFFEFEFE);
+const Color kPrimaryColor = Color(0xFFFFFFFF);
+const Color kSecondaryColor = Color(0xFF212121);
+const Color kdGrey = Color(0xFF838383);
+const Color kGrey = Color(0xFFE5E5E5);
+const Color kAccentColor = Color(0xFF1778F2);
