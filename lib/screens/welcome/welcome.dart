f59928e9e2f311ded7630e845fbd3253103aca93
Fabio-web <fabio.maccotta@ynov.com>
2021-12-31 02:58:37 +0100

    some update welcome page

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:music_player_app/constants/colors.dart';
 import 'package:music_player_app/custom_icons.dart';
+import 'package:music_player_app/screens/auth/login.dart';
 import 'package:music_player_app/widgets/button_builder.dart';
 
 class WelcomePage extends StatelessWidget {
@@ -8,18 +9,20 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          Container(
-              decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/images/white_desk_headphone.png"),
-                fit: BoxFit.cover),
-          )),
-          Container(
-            margin: EdgeInsets.only(left: 25, right: 25, top: 225),
-            child: Wrap(
-              runSpacing: 40,
+      body: Container(
+        padding: EdgeInsets.only(left: 25, right: 25, bottom: 50),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/white_desk_headphone.png"),
+              fit: BoxFit.cover,
+              alignment: Alignment.bottomCenter
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Wrap(
+              runSpacing: 50,
               alignment: WrapAlignment.center,
               children: [
                 Text("MY MUSIC", style: TextStyle(fontWeight: FontWeight.w800, fontSize: 50)),
@@ -38,7 +41,8 @@ class WelcomePage extends StatelessWidget {
                   runSpacing: 10,
                   children: [
                     ButtonBuilder("SING UP",
-                        color: kPrimaryColor, backgroundColor: kSecondaryColor),
+                        color: kPrimaryColor, backgroundColor: kSecondaryColor
+                    ),
                     ButtonBuilder("CONTINUE WITH FACEBOOK",
                         color: kPrimaryColor,
                         backgroundColor: kAccentColor,
@@ -48,16 +52,20 @@ class WelcomePage extends StatelessWidget {
                         backgroundColor: kGrey,
                         icon: CustomIcons.apple_icon),
                     ButtonBuilder("LOG IN",
-                        color: kSecondaryColor,
-                        backgroundColor: Colors.transparent,
-                        paddingVertical: 5),
+                      color: kSecondaryColor,
+                      backgroundColor: Colors.transparent,
+                      paddingVertical: 5,
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context) => LoginPage()));
+                      },
+                    ),
                   ],
                 )
               ],
             ),
-          )
-        ],
-      ),
+          ],
+        ),
+      )
     );
   }
 }
